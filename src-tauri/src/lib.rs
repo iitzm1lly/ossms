37a3cd6b2b930e1919386dc86c0df1718086153b
iitzm1lly <145307814+iitzm1lly@users.noreply.github.com>
2025-08-07 14:25:29 +0800

    Update lib.rs

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,10 +1,10 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
-    .setup(|app| {
+    .setup(|_app| {
       #[cfg(feature = "logging")]
       if cfg!(debug_assertions) {
-        app.handle().plugin(
+        _app.handle().plugin(
           tauri_plugin_log::Builder::default()
             .level(log::LevelFilter::Info)
             .build(),
